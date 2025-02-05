3525a2305dcba863481f94b8d807c11116dad81f
WhosDaMacc <142145705+WhosDaMacc@users.noreply.github.com>
2025-02-05 12:09:34 -0800

    Update scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -4,3 +4,10 @@ const navMenu = document.querySelector(".nav-menu");
 menuBtn.addEventListener("click", () => {  
   navMenu.classList.toggle("active");  
 });
+// Mobile Menu Toggle
+const menuBtn = document.querySelector(".menu-btn");
+const navMenu = document.querySelector(".nav-menu");
+
+menuBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
