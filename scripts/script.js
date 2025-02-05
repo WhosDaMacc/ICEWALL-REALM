10f1eb220bbb9445f2c675c6898280016801791b
WhosDaMacc <142145705+WhosDaMacc@users.noreply.github.com>
2025-02-05 11:53:51 -0800

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// Mobile Menu Toggle
+const menuBtn = document.querySelector('.menu-btn');
+const navMenu = document.querySelector('.nav-menu');
+
+menuBtn.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
