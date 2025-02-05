6383e5ebb0e147fca4413e7190d5721761ca0521
WhosDaMacc <142145705+WhosDaMacc@users.noreply.github.com>
2025-02-05 01:17:20 -0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,3 +21,15 @@ module.exports = {
     },
   },
 };
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+
+module.exports = {
+  solidity: "0.8.17", // Specify your Solidity version
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    // Add other networks here
+  },
+};
